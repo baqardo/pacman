1cdff6dcbb5d5fba9847382554559cd60c219828
MR-SKYFALL <46224840+MR-SKYFALL@users.noreply.github.com>
2020-04-24 19:57:20 +0200

    menu v3

--- menu.js
+++ menu.js
@@ -1,24 +1,53 @@
-// $(".play-button-js").click(function () {
-//     $(".maps-js").slideDown("slow", function () {
-//         $(this).css({
-//             height: "100px"
-//         });
-//     });
-// });
+$(".play-button-js").click(function () {
+    $(".menu__item").slideUp("slow", function () {
+        $(".test").slideDown("slow", function () {
 
-let play_buttons = document.querySelectorAll(".play-button-js");
-play_buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        let maps = document.querySelector(".maps-js");
-        if (maps.getAttribute("is_active") === "no") {
-            maps.style.height = "300px";
-            maps.setAttribute("is_active", "yes");
-        }
-        else {
-            maps.style.height = "0px";
-            maps.setAttribute("is_active", "no");
-        }
-    })
-})
+        })
+    });
+});
+
+// let play_buttons = document.querySelector(".play-button-js");
+
+// play_buttons.addEventListener("click", function () {
+//     let menu__items = document.querySelectorAll(".menu__item");
+//     menu__items.forEach(function (item) {
+//         item.classList.add("hide");
+
+
+//     })
+//     this.addEventListener("transitionend", function () {
+//         //  item.classList.add("no-display");
+//         console.log(this);
+//     })
+//     setTimeout(() => {
+
+
+//         let menu = document.querySelector(".menu");
+//         let maps = document.createElement("div");
+//         maps.setAttribute("class", "menu__items maps maps-js");
+//         maps.innerHTML = `
+//              <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             <div class="maps__items">
+//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
+//             </div>
+//             `
+//         menu.appendChild(maps);
+//     }, 500);
+// })
+
 
 
