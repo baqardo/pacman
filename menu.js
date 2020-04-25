fe6d909eb820087e82fb3ffed5343027696e570b
MR-SKYFALL <46224840+MR-SKYFALL@users.noreply.github.com>
2020-04-25 16:58:07 +0200

    full menu + RWD

--- menu.js
+++ menu.js
@@ -1,56 +1,61 @@
+let animation_speed = "medium";
+
 $(".play-button-js").click(function () {
-    $(".menu__item").slideUp("slow", function () {
-        $(".maps").slideDown("slow", function () {
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#maps").offset().top
-            }, 500);
+    $(".menu__item").slideUp(animation_speed, function () {
+        $(".maps").slideDown(animation_speed, function () {
+
+            $(this).css({
+                display: "flex"
+            })
+
 
         })
     });
 });
 
-// let play_buttons = document.querySelector(".play-button-js");
+$(".how-to-item-js").click(function () {
 
-// play_buttons.addEventListener("click", function () {
-//     let menu__items = document.querySelectorAll(".menu__item");
-//     menu__items.forEach(function (item) {
-//         item.classList.add("hide");
+    $(".menu__item").slideUp(animation_speed, function () {
 
+        $(".how-to-play").slideDown(animation_speed, function () {
 
-//     })
-//     this.addEventListener("transitionend", function () {
-//         //  item.classList.add("no-display");
-//         console.log(this);
-//     })
-//     setTimeout(() => {
+            $(this).css({
+                display: "flex"
+            })
 
 
-//         let menu = document.querySelector(".menu");
-//         let maps = document.createElement("div");
-//         maps.setAttribute("class", "menu__items maps maps-js");
-//         maps.innerHTML = `
-//              <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             <div class="maps__items">
-//                 <img class="maps__items__photo" src="./Images/slider/classic.png" alt="Classic Map">
-//             </div>
-//             `
-//         menu.appendChild(maps);
-//     }, 500);
-// })
+        })
+    });
+});
+
+$(".info-js").click(function () {
+
+    $(".menu__item").slideUp(animation_speed, function () {
+
+        $(".info").slideDown(animation_speed, function () {
+
+            $(this).css({
+                display: "flex"
+            })
 
 
+        })
+    });
+});
+
+
+
+
+$(".return-js").click(function () {
+    $(".maps,.how-to-play, .info").slideUp(animation_speed, function () {
+        $(".menu__item").slideDown(animation_speed, function () {
+
+            $(this).css({
+                display: "flex"
+            })
+
+
+        })
+    });
+});
 
