2dccf040c9d7284b6b9c849d7e633e3eb80ab1dc
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-04-26 23:01:11 +0200

    RWD NA LEVELACH!!!!

--- menu.js
+++ menu.js
@@ -1,61 +1,41 @@
-let animation_speed = "medium";
+let animation_speed = 'medium';
 
-$(".play-button-js").click(function () {
-    $(".menu__item").slideUp(animation_speed, function () {
-        $(".maps").slideDown(animation_speed, function () {
-
-            // $(this).css({
-            //     display: "flex"
-            // })
-
-
-        })
-    });
+$('.play-button-js').click(function() {
+	$('.menu__item').slideUp(animation_speed, function() {
+		$('.maps').slideDown(animation_speed, function() {
+			document.querySelector('.menu').classList.add('fixed-top');
+		});
+	});
 });
 
-$(".how-to-item-js").click(function () {
-
-    $(".menu__item").slideUp(animation_speed, function () {
-
-        $(".how-to-play").slideDown(animation_speed, function () {
-
-            $(this).css({
-                display: "flex"
-            })
-
-
-        })
-    });
+$('.how-to-item-js').click(function() {
+	$('.menu__item').slideUp(animation_speed, function() {
+		$('.how-to-play').slideDown(animation_speed, function() {
+			$(this).css({
+				display: 'flex'
+			});
+		});
+	});
 });
 
-$(".info-js").click(function () {
-
-    $(".menu__item").slideUp(animation_speed, function () {
-
-        $(".info").slideDown(animation_speed, function () {
-
-            $(this).css({
-                display: "flex"
-            })
-
-
-        })
-    });
+$('.info-js').click(function() {
+	$('.menu__item').slideUp(animation_speed, function() {
+		$('.info').slideDown(animation_speed, function() {
+			$(this).css({
+				display: 'flex'
+			});
+			document.querySelector('.menu').classList.add('fixed-top');
+		});
+	});
 });
 
-
-
-
-$(".return-js").click(function () {
-    $(".maps,.how-to-play, .info").slideUp(animation_speed, function () {
-        $(".menu__item").slideDown(animation_speed, function () {
-
-            $(this).css({
-                display: "flex"
-            })
-
-
-        })
-    });
+$('.return-js').click(function() {
+	$('.maps,.how-to-play, .info').slideUp(animation_speed, function() {
+		$('.menu__item').slideDown(animation_speed, function() {
+			$(this).css({
+				display: 'flex'
+			});
+			document.querySelector('.menu').classList.remove('fixed-top');
+		});
+	});
 });
-
